a463521ac866c2ef7380dd49e0be9f3852297c0b
alandas71 <alanalves.dev@gmail.com>
2025-06-12 09:03:29 -0300

    Feat: feito modificações

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { comingSoon } from "../../public";
 import Image from "next/image";
 
 export default function ComingSoon() {
@@ -7,7 +6,6 @@ export default function ComingSoon() {
   return ( 
     <div className="w-full flex flex-col items-center justify-center h-full">
       <h1>Coming Soon</h1>
-      <Image width={500} height={500} alt="comming soon" src={comingSoon}/>
     </div>
   );
 }
