dfccde13730acf134dfa6a3c6db993cc588d0a62
alandas71 <alanalves.dev@gmail.com>
2025-06-12 09:11:16 -0300

    Feat: feito modificações

--- src/components/common/logo.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Image from "next/image";
-import { logo } from "../../../public";
-const logoPath = "/logos/logo.png";
-
-const LoadingLogo = () => <img src={logoPath} alt="Logo" width={200} height={70} id="logo" />;
-
-export default LoadingLogo;
