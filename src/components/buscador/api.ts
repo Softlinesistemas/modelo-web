984e443cd4366d439a52bbab6e23d0df5004484e
alandas71 <alanalves.dev@gmail.com>
2025-07-22 09:43:10 -0300

    feat: feito retificação buscador3

--- src/components/buscador/api.ts
+++ src/components/buscador/api.ts
@@ -9,36 +9,42 @@ export const fetchMockResults = async (filters: any): Promise<BuscadorResultado[
           descricao: 'Agricultor familiar que vende produtos orgânicos',
           localizacao: 'São Paulo',
           tipo: filters.tipo || 'amigos',
+          id: ''
         },
         {
           nome: 'Grupo Agroecológico de Minas',
           descricao: 'Comunidade focada em agroflorestas',
           localizacao: 'Belo Horizonte',
           tipo: 'grupos',
+          id: ''
         },
         {
           nome: 'Máquinas Verdes',
           descricao: 'Venda e manutenção de tratores e colheitadeiras',
           localizacao: 'São Paulo',
-          tipo: 'fornecedor',
+          tipo: 'fornecedores',
+          id: ''
         },
         {
           nome: 'AgroSoluções',
           descricao: 'Serviços técnicos para irrigação e automação',
           localizacao: 'Goiás',
           tipo: 'clientes',
+          id: ''
         },
         {
           nome: 'Mercado do Campo',
           descricao: 'Cliente atacadista de produtos naturais',
           localizacao: '',
           tipo: 'clientes',
+          id: ''
         },
         {
           nome: 'Mercado do Campo',
           descricao: 'Cliente atacadista de produtos naturais',
           localizacao: '',
           tipo: 'clientes',
+          id: ''
         },
       ]);
     }, 500);
