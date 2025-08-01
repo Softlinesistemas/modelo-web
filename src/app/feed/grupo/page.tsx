7ec0fbea7a6feb15cd72b16103e921fceb87936c
alandas71 <alanalves.dev@gmail.com>
2025-08-01 13:08:23 -0300

    Test: tentando corrigir problema de approuter

--- src/app/feed/grupo/page.tsx
+++ src/app/feed/grupo/page.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import React from "react";
 import { BotaoAgrupado } from "@/components/feed/BotaoAgrupado";
 import { BotaoAlerta } from "@/components/feed/BotaoAlerta";
 import { FeedPhotoGallery } from "@/components/feed/FeedPhotoGallery";
