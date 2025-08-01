4570e0c56b213c47c13c86da722ee43d285050e7
Softlinesistemas <155544674+Softlinesistemas@users.noreply.github.com>
2025-08-01 13:48:13 -0300

    Update page.tsx

--- src/app/feed/grupo/page.tsx
+++ src/app/feed/grupo/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from "react";
 import { BotaoAgrupado } from "@/components/feed/BotaoAgrupado";
 import { BotaoAlerta } from "@/components/feed/BotaoAlerta";
 import { FeedPhotoGallery } from "@/components/feed/FeedPhotoGallery";
