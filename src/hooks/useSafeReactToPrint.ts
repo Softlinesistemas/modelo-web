a814794fa5bdac98862f2862b18e2a64d6b33494
alandas71 <alanalves.dev@gmail.com>
2025-08-08 09:03:19 -0300

    Fix

--- src/hooks/useSafeReactToPrint.ts
+++
@@ -1,9 +0,0 @@
-// hooks/useSafeReactToPrint.ts
-import { useReactToPrint } from "react-to-print";
-import type { UseReactToPrintOptions } from "react-to-print";
-
-export function useSafeReactToPrint(options: UseReactToPrintOptions) {
-  // Corrige tipagem do 'content'
-  const safeOptions = options as unknown as Parameters<typeof useReactToPrint>[0];
-  return useReactToPrint(safeOptions);
-}
